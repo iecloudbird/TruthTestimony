6eb9d6f432d6e54c1cd02d6edf2b6da616880262
iecloudbird <A00303759@student.tus.ie>
2024-07-08 13:04:51 +0100

    Created routes and models for user

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+//Variables declaration (etc. app, port, routes)
 const app = express();
 const port = process.env.PORT || 5000;
+const userRoutes = require('./routes/users');
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/users', userRoutes);
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
